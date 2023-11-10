ad20cd485c59b29aa5c779ce454772293de74814
none00y <mich_smyk@wp.pl>
2023-11-10 16:54:39 +0100

    add task no. 2

---
+++ Lab1_1/src/Hello.java
@@ -0,0 +1,9 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Hello {
+    //punkt wejściowy dla programu
+    public static void main(String[] args) {
+        //wypisujemy do konsoli
+        System.out.printf("Hello and welcome!");
+    }
+}
